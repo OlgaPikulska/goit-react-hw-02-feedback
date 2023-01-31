c7e55a3c4a25234b7d6a9bed9abe349ad81a6abc
Olga Pikulska <olga.pikulska.ld@gmail.com>
2023-01-31 23:31:47 +0100

    Add basic component and styles

---
+++ src/components/Feedback/Feedback.styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const StyledTitle = styled.h2`
+margin-left: 10px;
+`
+
+export const ButtonWrapper = styled.div`
+display: flex;
+flex-direction: row;
+gap: 10px;
+margin-left: 20px;
+`
+
+export const Button = styled.button`
+background-color: white;
+font-weight: bold;
+border: 1px solid #A5B2C5;
+border-radius: 4px;
+padding: 5px 8px 5px 8px;
+box-shadow: 1px 2px 2px 0px rgba(0,0,0,0.28);
+`
+export const StatisticsWrapper = styled.div`
+display: flex;
+flex-direction: column;
+margin-left: 20px;
+gap: 5px;
+`
+
+export const StatisticsItem = styled.span`
+font-weight: 600;
+`
