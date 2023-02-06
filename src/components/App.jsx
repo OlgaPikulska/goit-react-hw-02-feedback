922867f9974f20907f8d428171c966ba1252a545
Olga Pikulska <olga.pikulska.ld@gmail.com>
2023-02-06 17:04:46 +0100

    Refactor of App Component and add Notification

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,66 @@
-import { Feedback } from "./Feedback/Feedback";
+import React, { Component } from "react";
+import { Section } from "./Section"
+import { Statistics } from "./Statistics";
+import { FeedbackOptions } from "./FeedbackOptions";
+import { Notification } from "./Notification";
 
-export const App = () => {
-  return (
-    <>
-      <Feedback />
-    </>
-  );
+
+export class App extends Component {
+  static defaultProps = {
+    step: 1,
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  }
+
+  state = {
+    good: this.props.good,
+    neutral: this.props.neutral,
+    bad: this.props.bad,
+  }
+
+  handleFeedback = (e) => {
+    const { name } = e.currentTarget;
+    this.setState((state, props) => ({
+      [name]: state[name] + props.step,
+    }))
+  }
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    const totalFeedback = this.state.good + this.state.neutral + this.state.bad;
+    const positiveFeedback = Math.round(this.state.good * 100 / totalFeedback);
+    return totalFeedback === 0 ? totalFeedback : positiveFeedback;
+  }
+
+  render() {
+    const total = this.countTotalFeedback()
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.handleFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {total === 0
+            ? <Notification message="There is no feedback" />
+            : <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={total}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />}
+
+        </Section>
+      </>
+
+    );
+  }
 };
